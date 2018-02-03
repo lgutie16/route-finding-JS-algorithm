f03c9987674b6717765d51ec4cdb7c81473b0d29
lgutie16 <lindaandgod@gmail.com>
2018-02-03 17:33:22 -0500

    WIP destination point class

--- JS/script.js
+++ JS/script.js
@@ -4,10 +4,24 @@ window.onload = () =>{
     //Initial setup
     let startPoint = document.querySelector("[id='16']");
 
-    //Positioner
+        //Positioner
     let positioner = document.createElement('div');
     positioner.className = 'position';
 
-    //Place start point
+        //Place start point
     startPoint.append(positioner)
+
+        //Add event listener
+    const cells =  document.querySelectorAll('.cell')
+    //console.log("CELL",cells)
+    let destinationCell = startPoint
+    cells.forEach(function(cell) {
+        cell.addEventListener('click',() => {
+            destinationCell.classList.remove('destination')
+            cell.classList.add('destination')
+            destinationCell = cell
+        });
+    });
+
+    
 }
